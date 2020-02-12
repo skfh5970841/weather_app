824800ddeb0c164a9f962aed85f36c6886269574
skfh5970841 <leechangwan1012@gmail.com>
2020-02-12 23:05:16 +0900

    v1

--- App.js
+++ App.js
@@ -1,53 +1,66 @@
-import React from 'react';
-import {Alert} from 'react-native';
-import Loading from './Loading';
-import Weather from './Weather';
-import * as Location from 'expo-location';
-import axios from 'axios';
-const API_KEY = '31ce4e02ae903f138bd547365f90520e';
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import Weather from "./Weather";
+import * as Location from "expo-location";
+import axios from "axios";
+const API_KEY = "31ce4e02ae903f138bd547365f90520e";
 
-export default class extends React.Component{
-  state={
-    isLoading : true,
-    latitude : null,
-    longitude : null,
-    locationname : null,
-    temp : null,
-  }
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+    latitude: null,
+    longitude: null,
+    locationname: null,
+    temp: null,
+    description: null
+  };
 
-  getWeather = async(lat, lng) =>{
-   const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`);
-   this.setState({locationname: data.name});
-   this.setState({temp: data.main.temp});
-   //console.log(this.state.weather);
-  }
-  
-  getLocation = async () =>{
+  getWeather = async (lat, lng) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({ locationname: data.name });
+    this.setState({ temp: data.main.temp });
+    this.setState({ description: data.weather[0].description });
+    console.log(data.weather[0].description);
+  };
+
+  getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
-      this.setState({latitude, longitude});
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ latitude, longitude });
       this.getWeather(latitude, longitude);
-      this.setState({isLoading: false});
+      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find your position. Please ", "So Sad");
     }
-  }
-  
-  componentDidMount(){
+  };
+
+  componentDidMount() {
     this.getLocation();
   }
 
-  render(){
-    const {isLoading, latitude, longitude, locationname, temp} = this.state;
-    return (
-      isLoading 
-      ? <Loading/> 
-      : <Weather  
-        locationname = {locationname}
-        temp = {temp}
-    />
+  render() {
+    const {
+      isLoading,
+      latitude,
+      longitude,
+      locationname,
+      temp,
+      description
+    } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather
+        locationname={locationname}
+        temp={temp}
+        description={description}
+      />
     );
   }
 }
-
--- Weather.js
+++ Weather.js
@@ -1,42 +1,110 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import PropTypes from "prop-types";
 
+import cloud from "./weatherImg/cloud.png";
+import dark from "./weatherImg/dark.png";
+import drizzle from "./weatherImg/drizzle.png";
+import dust from "./weatherImg/dust.png";
+import fog from "./weatherImg/fog.png";
+import rain from "./weatherImg/rain.png";
+import smoke from "./weatherImg/smoke.png";
+import snow from "./weatherImg/snow.png";
+import squall from "./weatherImg/squall.png";
+import sunny from "./weatherImg/sunny.png";
+import thunderstorm from "./weatherImg/thunderstorm.png";
+import tornado from "./weatherImg/tornado.png";
 
-function Weather({locationname, temp}){
-    return (
-    <View style={styles.container}>
-        <Text style={styles.loadingText}>{locationname} {temp}</Text>
-    </View>
-);
+import clearbg from "./backrounImg/clearbg.jpg";
+import cloudbg from "./backrounImg/cloudbg.jpg";
+import defaultImg from "./backrounImg/defaultImg.jpg";
+import drizzlebg from "./backrounImg/drizzlebg.jpg";
+import dustbg from "./backrounImg/dustbg.jpg";
+import mistbg from "./backrounImg/mistbg.jpg";
+import rainybg from "./backrounImg/rainybg.jpg";
+import smokebg from "./backrounImg/smokebg.jpg";
+import snowybg from "./backrounImg/snowybg.jpg";
+import squallbg from "./backrounImg/squallbg.jpg";
+import thunderstormbg from "./backrounImg/thunderstormbg.jpg";
+import tornadobg from "./backrounImg/tornadobg.jpg";
+
+function weatherDiv(description) {
+  try {
+    if (description.match("rain") != null) return rain;
+    else if (description.match("snow") != null) return snow;
+    else if (description.match("clear") != null) return sunny;
+    else if (description.match("thunder") != null) return thunderstorm;
+    else if (description.match("drizzle") != null) return drizzle;
+    else if (
+      description.match("mist") ||
+      description.match("fog") ||
+      description.match("haze") != null
+    )
+      return fog;
+    else if (description.match("smoke") != null) return smoke;
+    else if (description.match("dust") || description.match("sand") != null)
+      return dust;
+    else if (description.match("squall") != null) return squall;
+    else if (description.match("tornado") != null) return tornado;
+    else if (description.match("cloud") != null) return cloud;
+    else return dark;
+  } catch (error) {
+    console.log(error);
+  }
+  return dark;
 }
-/*
-class Weather extends React.Component{
-    state={
+function bgImg(description) {
+  try {
+    if (description === rain) return rainybg;
+    else if (description === snow) return snowybg;
+    else if (description === clear) return clearbg;
+    else if (description === thunderstorm) return thunderstormbg;
+    else if (description === drizzle) return drizzlebg;
+    else if (description === fog) return mistbg;
+    else if (description === smoke) return smokebg;
+    else if (description === dust) return dustbg;
+    else if (description === squall) return squallbg;
+    else if (description === tornado) return tornadobg;
+    else if (description === cloud) return cloudbg;
+    else return defaultImg;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    }
-    render(){
-        return (
-            <View style={styles.container}>
-                <Text>weather Page</Text>
-            </View>
-        );
-    }
-}*/
+function Weather({ locationname, temp, description }) {
+  const WeatherImg = weatherDiv(description);
+  const bg = bgImg(WeatherImg);
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground style={styles.background} source={bg}></ImageBackground>
+      <Image style={{ width: 50, height: 50 }} source={WeatherImg} />
+      <Text style={styles.defaulttext}>city: {locationname}</Text>
+      <Text style={styles.defaulttext}>temp: {temp}</Text>
+      <Text style={styles.defaulttext}>weather: {description}</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingHorizontal: 30,
-        paddingVertical: 100,
-        backgroundColor: '#FDF6AA',
-        padding: 20
-    },
-    loadingText:{
-        color: '#2c2c2c',
-        fontSize: 30,
-    }
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#c2f2f2",
+    padding: 20
+  },
+  defaulttext: {
+    color: "#2c2c2c",
+    fontSize: 30
+  }
 });
-
 
 export default Weather;
